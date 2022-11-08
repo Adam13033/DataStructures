82d457e57d092355e7c1a4c4da162f9026ef30ac
AdamLemdani <adamlemdani11@googlemail.com>
2022-11-08 21:57:20 +0000

    Changed BST

--- src/main/java/com/example/datastructures/BinarySearchTree/BST.java
+++ src/main/java/com/example/datastructures/BinarySearchTree/BST.java
@@ -1,172 +1,76 @@
 package com.example.datastructures.BinarySearchTree;
 
-import java.util.Stack;
-
 public class BST {
 
 	private Node root;
 
-	public void insert (int key, String value) {
-		Node newNode = new Node(key, value);
-
+	public void insert(int value) {
 		if (root == null) {
-			root = newNode;
-		} else {
-			Node currentNode = root;
-			Node parentNode;
-
-			while (true) {
-				parentNode = currentNode;
-				if (key < currentNode.key) {
-					currentNode = currentNode.left;
-					if (currentNode == null) {
-						parentNode.left = newNode;
-						return;
-					}
-				} else {
-					currentNode = currentNode.right;
-					if (currentNode == null) {
-						parentNode.right = newNode;
-						return;
-					}
-				}
-			}
+			root = new Node(value);
+		}
+		else {
+			root.insert(value);
 		}
 	}
 
-	public Node findMinimum () {
-		Node currentNode = root;
-		Node lastNode = null;
-
-		while (currentNode != null) {
-			lastNode = currentNode;
-			currentNode = currentNode.left;
+	public Node get(int value) {
+		if (root != null) {
+			return root.get(value);
 		}
-		return lastNode;
+
+		return null;
 	}
 
-	public Node findMaximum () {
-		Node currentNode = root;
-		Node lastNode = null;
-
-		while (currentNode != null) {
-			lastNode = currentNode;
-			currentNode = currentNode.right;
-		}
-		return lastNode;
+	public void delete(int value) {
+		root = delete(root, value);
 	}
 
-	public boolean deleteNode (int key) {
-		Node currentNode = root;
-		Node parentNode = root;
+	private Node delete(Node subtreeRoot, int value) {
+		if (subtreeRoot == null) {
+			return subtreeRoot;
+		}
 
-		boolean leftChild = false;
-
-		while (currentNode.key != key) {
-			parentNode = currentNode;
-			if (key < currentNode.key) {
-				leftChild = true;
-				currentNode = currentNode.left;
-			} else {
-				currentNode = currentNode.right;
-				leftChild = false;
+		if (value < subtreeRoot.getData()) {
+			subtreeRoot.setLeftChild(delete(subtreeRoot.getLeftChild(), value));
+		}
+		else if (value > subtreeRoot.getData()) {
+			subtreeRoot.setRightChild(delete(subtreeRoot.getRightChild(), value));
+		}
+		else {
+			// Cases 1 and 2: node to delete has 0 or 1 child(ren)
+			if (subtreeRoot.getLeftChild() == null) {
+				return subtreeRoot.getRightChild();
 			}
-			if (currentNode == null) {
-				return false;
+			else if (subtreeRoot.getRightChild() == null) {
+				return subtreeRoot.getLeftChild();
 			}
 		}
 
-		Node nodeToDelete = currentNode;
-
-		if (nodeToDelete.left == null && nodeToDelete.right == null) {
-			if (nodeToDelete == root) {
-				root = null;
-			} else if (leftChild) {
-				parentNode.left = null;
-			} else {
-				parentNode.right = null;
-			}
-		} else if (nodeToDelete.left == null) {
-			if (nodeToDelete == root) {
-				root = nodeToDelete.right;
-			} else if (leftChild) {
-				parentNode.left = nodeToDelete.right;
-			} else {
-				parentNode.right = nodeToDelete.right;
-			}
-		} else {
-			Node successor = getSuccessor(nodeToDelete);
-			if (nodeToDelete == root) {
-				root = successor;
-			} else if (leftChild) {
-				parentNode.left = successor;
-			} else {
-				parentNode.right = successor;
-			}
-			successor.left = nodeToDelete.left;
-		}
-		return true;
+		return subtreeRoot;
 	}
 
-	private Node getSuccessor (Node nodeToDelete) {
-		Node parent = nodeToDelete;
-		Node successor = nodeToDelete;
-
-		Node currentNode = nodeToDelete.right;
-
-		while (currentNode != null) {
-			parent = successor;
-			successor = currentNode;
-			currentNode = currentNode.left;
+	public int min() {
+		if (root == null) {
+			return Integer.MIN_VALUE;
 		}
-		if (successor != nodeToDelete.right) {
-			parent.left = successor.right;
-			successor.right = nodeToDelete.right;
+		else {
+			return root.min();
 		}
-		return successor;
 	}
 
-	public void displayTree () {
-		Stack globalStack = new Stack();
-		globalStack.push(root);
-		int nBlanks = 32;
-		boolean isRowEmpty = false;
-		System.out.println("......................................................");
-
-		while (isRowEmpty == false) {
-			Stack localStack = new Stack();
-			isRowEmpty = true;
-			for (int j = 0; j < nBlanks; j++) {
-				System.out.print(" ");
-			}
-
-			while (globalStack.isEmpty() == false) {
-				Node temp = (Node) globalStack.pop();
-				if (temp != null) {
-					System.out.print(temp.key);
-					localStack.push(temp.left);
-					localStack.push(temp.right);
-					if (temp.left != null || temp.right != null) {
-						isRowEmpty = false;
-					}
-				} else {
-					System.out.print("--");
-					localStack.push(null);
-					localStack.push(null);
-				}
-
-				for (int j = 0; j < nBlanks * 2 - 2; j++) {
-					System.out.print(" ");
-				}
-			}
-
-			System.out.println();
-			nBlanks = nBlanks / 2;
-
-			while (localStack.isEmpty() == false)
-				globalStack.push(localStack.pop());
+	public int max() {
+		if (root == null) {
+			return Integer.MAX_VALUE;
 		}
-		System.out.println("......................................................");
-
+		else {
+			return root.max();
+		}
 	}
+
+	public void traverseInOrder() {
+		if (root != null) {
+			root.traverseInOrder();
+		}
+	}
+
 }
--- src/main/java/com/example/datastructures/BinarySearchTree/Node.java
+++ src/main/java/com/example/datastructures/BinarySearchTree/Node.java
@@ -2,14 +2,109 @@ package com.example.datastructures.BinarySearchTree;
 
 public class Node {
 
-	int key;
-	String value;
-	Node left, right;
+	private int data;
+	private Node leftChild;
+	private Node rightChild;
 
-	public Node(int key, String value) {
-		super();
-		this.key = key;
-		this.value = value;
+	public Node (int data) {
+		this.data = data;
 	}
 
+	public void insert(int value) {
+		if (value == data) {
+			return;
+		}
+
+		if (value < data) {
+			if (leftChild == null) {
+				leftChild = new Node(value);
+			}
+			else {
+				leftChild.insert(value);
+			}
+		}
+		else {
+			if (rightChild == null) {
+				rightChild = new Node(value);
+			}
+			else {
+				rightChild.insert(value);
+			}
+		}
+	}
+
+	public Node get(int value) {
+		if (value == data) {
+			return this;
+		}
+
+		if (value < data) {
+			if (leftChild != null) {
+				return leftChild.get(value);
+			}
+		}
+		else {
+			if (rightChild != null) {
+				return rightChild.get(value);
+			}
+		}
+
+		return null;
+	}
+
+	public int min() {
+		if (leftChild == null) {
+			return data;
+		}
+		else {
+			return leftChild.min();
+		}
+	}
+
+	public int max() {
+		if (rightChild == null) {
+			return data;
+		}
+		else {
+			return rightChild.max();
+		}
+	}
+
+	public void traverseInOrder() {
+		if (leftChild != null) {
+			leftChild.traverseInOrder();
+		}
+		System.out.print(data + ", ");
+		if (rightChild != null) {
+			rightChild.traverseInOrder();
+		}
+	}
+
+	public int getData() {
+		return data;
+	}
+
+	public void setData(int data) {
+		this.data = data;
+	}
+
+	public Node getLeftChild() {
+		return leftChild;
+	}
+
+	public void setLeftChild(Node leftChild) {
+		this.leftChild = leftChild;
+	}
+
+	public Node getRightChild() {
+		return rightChild;
+	}
+
+	public void setRightChild(Node rightChild) {
+		this.rightChild = rightChild;
+	}
+
+	public String toString() {
+		return "Data = " + data;
+	}
 }
